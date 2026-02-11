9b517a57c770d005a1ca173e0f55c684b47e7181
Luiz Eduardo <ltibes925@gmail.com>
2026-02-11 16:20:38 -0300

    Exercicio 3#

---
+++ Personagem.js
@@ -0,0 +1,9 @@
+let Nome = "Neuvilette";
+let Visao = "Hydro";
+let Vida = "32947";
+let Stamina = "8";
+
+console.log("Nome do Personagem: " + Nome);
+console.log("Seu Elemento é: " + Visao);
+console.log("Vida Atual: " + Vida);
+console.log("Stamina Atual: " + Stamina);
