0b6e6d3111058679108e6c0461ef32a9bfbb0309
Luiz Eduardo <ltibes925@gmail.com>
2026-02-12 14:48:26 -0300

    Exercicio 5#

---
+++ CalculadoraTerminal.js
@@ -0,0 +1,9 @@
+
+const numero1 = Number(process.argv[2]);
+const numero2 = Number(process.argv[3]);
+
+const soma = numero1 + numero2
+const multiplicacao = numero1 * numero2
+
+console.log("Soma:", soma);
+console.log("Multiplicação:", multiplicacao);
